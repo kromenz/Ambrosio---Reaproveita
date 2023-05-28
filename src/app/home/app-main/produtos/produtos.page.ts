a446a8be2fc4f877fb6ea5eb415d608f392de07c
kromenz <rafael.pires.andre@ipvc.pt>
2023-05-28 15:58:15 +0100

    filtros a dar

--- src/app/home/app-main/produtos/produtos.page.ts
+++ src/app/home/app-main/produtos/produtos.page.ts
@@ -8,25 +8,23 @@ interface Prods {
   categoria: string;
 };
 
-/* car_pes_ovo é carne, pescados e ovos */
-
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.page.html',
   styleUrls: ['./produtos.page.scss'],
 })
 
-export class ProdutosPage implements OnInit{
+export class ProdutosPage implements OnInit {
   @ViewChild('popover')
-  popover!: { event: Event; };
-  public dataProds: Prods [] = []
+  popover!: { event: Event };
+  public dataProds: Prods[] = [];
   
   public searchTerm: string = "";
 
   constructor(private router: Router) { }
 
   ngOnInit() {
-    this.loadProds()
+    this.loadProds();
   }
   isOpen = false;
 
@@ -36,14 +34,14 @@ export class ProdutosPage implements OnInit{
   }
 
   onClick(x: any){
-    this.router.navigateByUrl(x)
+    this.router.navigateByUrl(x);
   }
 
   loadProds(){
     fetch('./assets/data/prods.json')
       .then((response) => response.json())
       .then((json) => {
-        if(Array.isArray(json)){
+        if (Array.isArray(json)) {
           this.dataProds = json;
           console.log('list data loaded', this.dataProds);
         } else {
@@ -54,4 +52,63 @@ export class ProdutosPage implements OnInit{
       });
   }
 
+  sortByDate() {
+    this.dataProds.sort((a, b) => {
+      const datePartsA = a.data.split('/');
+      const dateA = new Date(parseInt(datePartsA[2]), parseInt(datePartsA[1]) - 1, parseInt(datePartsA[0]));
+      
+      const datePartsB = b.data.split('/');
+      const dateB = new Date(parseInt(datePartsB[2]), parseInt(datePartsB[1]) - 1, parseInt(datePartsB[0]));
+      
+      if (dateA < dateB) {
+        return -1;
+      } else if (dateA > dateB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  
+    console.log('Data ordenada:', this.dataProds);
+  }
+  
+  
+  sortAlphabetically() {
+    this.dataProds.sort((a, b) => a.nome.localeCompare(b.nome));
+  }
+
+  sortByQuantity() {
+    this.dataProds.sort((a, b) => {
+      const quantidadeA = this.extractNumericValue(a.quantidade);
+      const quantidadeB = this.extractNumericValue(b.quantidade);
+      return quantidadeA - quantidadeB;
+    });
+  
+    console.log('Quantidade ordenada:', this.dataProds);
+  }
+  
+  extractNumericValue(quantidade: string): number {
+    const regex = /(\d+(\.\d+)?)\s*(g|kg|ml|L|unidade|unidades)/i; // Atualização da expressão regular
+    const match = quantidade.match(regex);
+    if (match) {
+      const numericValue = parseFloat(match[1]);
+      const unit = match[3].toLowerCase();
+      switch (unit) {
+        case 'g':
+          return numericValue / 1000;
+        case 'ml':
+          return numericValue / 1000;
+        case 'kg':
+          return numericValue;
+        case 'l':
+          return numericValue;
+        case 'unidade':
+          return numericValue;
+        case 'unidades':
+          return numericValue;
+      }
+    }
+    return 0; // Retornar 0 se não houver um valor numérico válido
+  }  
+   
 }
