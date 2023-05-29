a101aca7460e451dde67ee8311e13ef9567bdc8e
kromenz <rafael.pires.andre@ipvc.pt>
2023-05-29 14:21:43 +0100

    Finalizado

--- src/app/home/app-main/lista-compras/lista-compras.page.ts
+++ src/app/home/app-main/lista-compras/lista-compras.page.ts
@@ -1,29 +1,24 @@
 import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Router } from '@angular/router';
-import { ScreenOrientation, OrientationLockOptions } from '@capacitor/screen-orientation';
+import {
+  ScreenOrientation,
+  OrientationLockOptions,
+} from '@capacitor/screen-orientation';
 import { ViewWillEnter } from '@ionic/angular';
-
-interface Produto {
-  nome: string;
-  visivel: boolean;
-}
 
 @Component({
   selector: 'app-lista-compras',
   templateUrl: 'lista-compras.page.html',
-  styleUrls: ['lista-compras.page.scss']
+  styleUrls: ['lista-compras.page.scss'],
 })
-export class ListaComprasPage implements ViewWillEnter{
+export class ListaComprasPage implements ViewWillEnter {
   voltandoAtras: boolean = false;
 
-  produtosSelecionados: Produto[] = [];
-  produtosListaOriginal: Produto[] = []; // Nova propriedade para armazenar a lista original de produtos selecionados
+  produtosSelecionados: string[] = [];
+  produtosListaOriginal: string[] = []; // Nova propriedade para armazenar a lista original de produtos selecionados
 
   novoProduto: string = '';
-
-  produtosListaCompras: Produto[] = [];
-
 
   constructor(private storage: Storage, private router: Router) {
     this.initializeStorage(); // Chame o método initializeStorage() no construtor
@@ -36,43 +31,31 @@ export class ListaComprasPage implements ViewWillEnter{
 
   ionViewWillEnter() {
     this.carregarSelecionados();
-  
-    // Verificar se há produtos em produtosListaCompras que não estão em produtosSelecionados
-    this.produtosListaCompras.forEach(produto => {
-      const existe = this.produtosSelecionados.some(p => p.nome === produto.nome);
-      if (!existe) {
-        produto.visivel = false;
-      }
-    });
 
     const options: OrientationLockOptions = { orientation: 'portrait' };
     ScreenOrientation.lock(options);
   }
-  
+
   onClick(x: any) {
     this.router.navigateByUrl(x);
   }
 
   carregarSelecionados() {
-    this.storage.get('produtosSelecionados').then((produtos: Produto[]) => {
+    this.storage.get('produtosSelecionados').then((produtos: string[]) => {
       if (produtos) {
         this.produtosSelecionados = produtos;
-  
+
         // Verificar se algum produto da produtosListaOriginal não está presente em produtosSelecionados e removê-los
-        this.produtosListaOriginal = this.produtosListaOriginal.filter(p =>
-          this.produtosSelecionados.some(s => s.nome === p.nome)
+        this.produtosListaOriginal = this.produtosListaOriginal.filter((p) =>
+          this.produtosSelecionados.some((s) => s === p)
         );
-  
-        this.atualizarProdutosListaCompras();
       }
     });
   }
-  
 
   adicionarProduto() {
     if (this.novoProduto) {
-      const novoProduto: Produto = { nome: this.novoProduto, visivel: true };
-      this.produtosSelecionados.push(novoProduto);
+      this.produtosSelecionados.push(this.novoProduto);
       this.novoProduto = '';
       this.salvarSelecionados();
     }
@@ -80,23 +63,17 @@ export class ListaComprasPage implements ViewWillEnter{
 
   salvarSelecionados() {
     this.storage.set('produtosSelecionados', this.produtosSelecionados);
-    this.atualizarProdutosListaCompras();
   }
 
-  removerProduto(produto: Produto) {
-    this.produtosSelecionados = this.produtosSelecionados.filter(p => p !== produto);
-    this.produtosListaCompras = this.produtosListaCompras.filter(p => p !== produto); // Atualize a lista produtosListaCompras
+  removerProduto(produto: string) {
+    this.produtosSelecionados = this.produtosSelecionados.filter(
+      (p) => p !== produto
+    );
     this.salvarSelecionados();
-  }
-  
-  private atualizarProdutosListaCompras() {
-    this.produtosListaCompras = this.produtosSelecionados.filter(produto => produto.visivel);
   }
 
   restaurarEstadoOriginal() {
     this.produtosSelecionados = [...this.produtosListaOriginal]; // Restaure a lista de produtos selecionados com a lista original
-    this.produtosListaCompras = this.produtosSelecionados.filter(produto => produto.visivel); // Atualize a lista produtosListaCompras
     this.salvarSelecionados();
   }
-
 }
